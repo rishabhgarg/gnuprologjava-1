9516d8375c10b804c8e08af3edfb9fb12d06bb17
Rishabh Garg <rishabhgarg.as@gmail.com>
2013-07-28 01:02:36 +0530

    Add note about modifying the parser to support unicode and using Javacc 5

--- src/gnu/prolog/io/parser/package-info.java
+++ src/gnu/prolog/io/parser/package-info.java
@@ -1,23 +1,23 @@
 /**
- * This package contains the TermParser.jj file which is used by javacc to
+ * This package contains the TermParser.jj file which is used by javacc 5 to
  * generate the parser found in {@link gnu.prolog.io.parser.gen}.
  * 
- * Unfortunately modern versions of javacc cannot be used to regenerate the
- * parser as the parser becomes non-terminating. When you wish to modify
- * something in .gen then you need to also modify the .jj file - if you can't
- * reflect your changes there then don't make those changes.
- * 
- * There is also a TermParser.g file which is an in progress conversion of
- * JavaCC grammar to antlr.
+ * You only need to make changes in the TermParser.jj file which will reflect
+ * the changes automatically in the .gen files and you are done. However, on 
+ * using javacc TermParser.jj, you need to change the JavaCharStream.java and
+ * change the first line of the code to 
+ * class JavaCharStream implements CharStream
+ *
+ * There is also a parser branch on github which is the rewriting of parser
+ * in ANTLR 4 but ANTLR 4 does not support left-recursion with parameters
  * 
  * This package also contains classes such as
  * {@link gnu.prolog.io.parser.NameToken NameToken},
- * {@link gnu.prolog.io.parser.ReaderCharStream ReaderCharStream} and
+ * {@link gnu.prolog.io.parser.JavaCharStream JavaCharStream} and
  * {@link gnu.prolog.io.parser.TermParserUtils TermParserUtils} which contain
  * some of the code needed to integrate the generated parser with the rest of
  * the codebase.
  * 
- * TODO: parser does not properly handle unicode even in comments @see ReaderCharStream
  */
 package gnu.prolog.io.parser;
 
